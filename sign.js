fc39093369dbb9abcc5e3d064747151d0db54aaa
EvanOkot <evanceokoth199@gmail.com>
2024-05-03 14:23:00 +0300

    Add files via upload

---
+++ sign.js
@@ -0,0 +1,5 @@
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for signing up!');
+    // Additional code for handling the signup logic
+});
